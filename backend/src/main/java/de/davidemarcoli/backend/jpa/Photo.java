09f2a37e62b3a9c5e1ca4baef564aa3af643cb9d
davidemarcoli <davide@marcoli.ch>
2022-09-20 14:07:24 +0200

    Implement JWT Token

--- backend/src/main/java/de/davidemarcoli/backend/jpa/Photo.java
+++ backend/src/main/java/de/davidemarcoli/backend/jpa/Photo.java
@@ -1,8 +1,8 @@
 package de.davidemarcoli.backend.jpa;
 
-import org.springframework.data.mongodb.core.mapping.Document;
+//import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "photos")
+//@Document(collection = "photos")
 public class Photo {
     private String filename;
     private byte[] photo;
---
+++ backend/src/main/java/de/davidemarcoli/backend/security/jwt/JwtRequest.java
@@ -0,0 +1,18 @@
+package de.davidemarcoli.backend.security.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class JwtRequest implements Serializable {
+
+    private static final long serialVersionUID = 5926468583005150707L;
+
+    private String username;
+    private String password;
+}
