53b84dced3e55711691990570fc842b64b28e674
davidemarcoli <davide@marcoli.ch>
2022-10-04 18:02:53 +0200

    Add first search prototype

--- frontend/src/app/forms/post/list-posts/list-posts.component.ts
+++ frontend/src/app/forms/post/list-posts/list-posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PostService} from "../../../services/post/post.service";
 import {Post} from "../../../models/post";
 import {Router} from "@angular/router";
@@ -12,10 +12,12 @@ import * as moment from "moment";
 export class ListPostsComponent implements OnInit {
 
   posts: Post[] = [];
+  searchedPosts: { post: Post, importance: number }[] = [];
 
   constructor(private postService: PostService, private router: Router) {
     this.postService.getPosts().toPromise().then(value => {
       this.posts = value || [];
+      this.search()
     })
   }
 
@@ -25,6 +27,29 @@ export class ListPostsComponent implements OnInit {
 
   getRelativeDate(date: Date) {
     return moment(date).fromNow();
+  }
+
+  search(event?: any) {
+    if (event) {
+      const searchTerm = event.data.toUpperCase();
+      console.log(event)
+
+      const newSearchedPosts = [];
+
+      this.posts.forEach(post => {
+        const isInCategories = post.categories.filter(category => {
+          return category.name.toUpperCase().includes(searchTerm)
+        }).length > 0;
+        const isInTitle = post.title.toUpperCase().includes(searchTerm)
+        if (isInTitle) newSearchedPosts.push({post: post, importance: 3})
+        if (isInCategories) newSearchedPosts.push({post: post, importance: 2})
+        // const isInContent = true;
+      })
+    } else {
+      this.searchedPosts = this.posts.map(value => {
+        return {post: value, importance: 3}
+      });
+    }
   }
 
   ngOnInit(): void {
